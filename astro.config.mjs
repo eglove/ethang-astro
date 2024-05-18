44239b04e81dc4f3e4d4eb1bf4e830b9e5a50b61
Ethan Glover <hello@ethang.email>
2024-05-17 22:52:43 -0500

    spec

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  integrations: [serviceWorker()]
 });

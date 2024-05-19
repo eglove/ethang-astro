8a9d309f5624c5aa212c7566d6ac557e719a7138
Ethan Glover <hello@ethang.email>
2024-05-19 15:10:50 -0500

    used

---
+++ src/components/resume/utils/used.ts
@@ -0,0 +1,29 @@
+import map from "lodash/map";
+import { positions } from "./positions.ts";
+
+export function getAllUsed() {
+  const methodologiesUsed = map(positions, (position) => {
+    return position.methodologiesUsed;
+  })
+    .flat()
+    .sort((a, b) => {
+      return a.localeCompare(b);
+    });
+
+  const techUsed = map(positions, (position) => {
+    return position.techUsed;
+  })
+    .flat()
+    .sort((a, b) => {
+      return a.localeCompare(b);
+    });
+
+  return [methodologiesUsed, techUsed];
+}
+
+export function getUsedByPosition(position: keyof typeof positions) {
+  const methodologiesUsed = positions[position].methodologiesUsed;
+  const techUsed = positions[position].techUsed;
+
+  return [methodologiesUsed, techUsed];
+}

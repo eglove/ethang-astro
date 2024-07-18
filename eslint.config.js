35802e7781995219b452d310d093e936a3db43ba
Ethan Glover <hello@ethang.email>
2024-07-18 11:45:51 -0500

    Update Commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import config from "@ethang/eslint-config/eslint.config.js";
 import tseslint from "typescript-eslint";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 export default tseslint.config(...config, {
   ignores: ["dist/"],
   languageOptions: {

739dfb213f975bddf8f7a382e6ebeaa7697f04d1
Ethan Glover <hello@ethang.email>
2024-08-03 22:35:32 -0500

    add astro config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import config from "@ethang/eslint-config/eslint.config.js";
 import tseslint from "typescript-eslint";
+import configAstro from "@ethang/eslint-config-astro";
 
-export default tseslint.config(...config, {
+export default tseslint.config(...config, ...configAstro, {
   ignores: ["dist/"],
   languageOptions: {
     parserOptions: {
